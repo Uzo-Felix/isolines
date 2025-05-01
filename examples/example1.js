ce6855517e66e035b70990155f7ffe18b3cec740
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2025-05-01 12:44:14 +0300

    chore: sample

---
+++ examples/example1.js
@@ -0,0 +1,14 @@
+const isolines = require('../src/index');
+
+const values = [
+  10, 20, 30,
+  40, 50, 60,
+  70, 80, 90
+];
+
+const geojson = isolines.generateIsolinesFromValues(values, {
+  width: 3,
+  height: 3
+});
+
+console.log(JSON.stringify(geojson, null, 2));
